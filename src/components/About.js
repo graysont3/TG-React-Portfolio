3ae1ee7b0c35783d9f5386da4b0c566b078c5d1d
Star Child <tajagrayson@Tajas-MacBook-Air.local>
2023-07-22 10:13:23 -0400

    added more files

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,12 @@
 import React from "react";
-import Image from 'react-bootstrap/Image' 
-import '../css'
+import myImage from './'
+
 
 function About() {
     return (
 <div>
     <h2 id="about">About Me</h2>
-    <Col xs={6} md={4}> <Image src="holder.js/171x180" rounded /></Col>
+    <Col xs={6} md={4}> <Image src={myImage} rounded /></Col>
       <p id="about-me">
      My name is Taja Grayson and I am from Columbia, SC. I have a Bachelors in Biology from Winthrop University and am currently working as Laboratory scientist.
      2 years in I was ready to change my career, which lead me to taking the full stack web development course.
@@ -15,3 +15,5 @@ function About() {
 </div>
     )
 };
+
+export default About;
